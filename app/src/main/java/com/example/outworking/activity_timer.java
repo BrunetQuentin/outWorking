ad11ebe11953069889ac12fafa45661f1da5ccb5
quentin <brunetq38@gmail.com>
2022-11-22 16:25:51 +0100

    create workout

--- app/src/main/java/com/example/outworking/activity_timer.java
+++ app/src/main/java/com/example/outworking/activity_timer.java
@@ -1,10 +1,13 @@
 package com.example.outworking;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -12,14 +15,22 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.outworking.db.Workout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class activity_timer extends AppCompatActivity {
 
     Workout workout;
 
+    String[] keys = new String[] { "Prepare", "Work", "Rest", "Cycles", "Sets", "Rest between sets", "Cool Down" };
+
+    HashMap<String, Integer> ids;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +45,79 @@ public class activity_timer extends AppCompatActivity {
 
         LinearLayout timer = (LinearLayout) findViewById(R.id.activity_timer);
 
-        HashMap<String, Integer> map = new HashMap<String, Integer>(){{
-            put("Prepare", workout.getPrepare());
-            put("Work", workout.getWork());
-            put("Rest", workout.getRest());
-            put("Cycles", workout.getCycles());
-            put("Sets", workout.getSets());
-            put("Rest between sets", workout.getRestBetweenSets());
-            put("Cool Down", workout.getCoolDown());
-        }};
+        ids = new HashMap<String, Integer>();
 
-        for (String key : map.keySet()) {
+        for (String key : keys) {
             LayoutInflater inflater = LayoutInflater.from(activity_timer.this); // 1
             View line = inflater.inflate(R.layout.template_timer, null);
 
             TextView title = (TextView) line.findViewById(R.id.title);
             title.setText(key);
 
+            FloatingActionButton buttonAdd = (FloatingActionButton) line.findViewById(R.id.plus);
+            FloatingActionButton buttonMinus = (FloatingActionButton) line.findViewById(R.id.minus);
+
             EditText text = (EditText) line.findViewById(R.id.editNumber);
-            System.out.println(map.get(key));
-            text.setText(map.get(key).toString());
+
+            int id = View.generateViewId();
+            ids.put(key, id);
+            text.setId(id);
+
+            buttonAdd.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    text.setText(Integer.toString(Integer.parseInt(String.valueOf(text.getText())) + 1));
+                }
+            });
+
+            buttonMinus.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    text.setText(Integer.toString(Integer.parseInt(String.valueOf(text.getText())) - 1));
+                }
+            });
+            text.setText(Integer.toString(getValue(key)));
 
             timer.addView(line);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void modifyValue(String key, int number) {
+
+        HashMap<String, Runnable> map = new HashMap<String, Runnable>(){{
+            put("Prepare", () -> workout.setPrepare(number));
+            put("Work", () -> workout.setWork(number));
+            put("Rest", () -> workout.setRest(number));
+            put("Cycles", () -> workout.setCycles(number));
+            put("Sets", () -> workout.setSets(number));
+            put("Rest between sets", () -> workout.setRestBetweenSets(number));
+            put("Cool Down", () -> workout.setCoolDown(number));
+        }};
+
+        map.get(key).run();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public int getValue(String key) {
+        HashMap<String, Supplier<Integer>> map = new HashMap<String, Supplier<Integer>>(){{
+            put("Prepare", () -> workout.getPrepare());
+            put("Work", () -> workout.getWork());
+            put("Rest", () -> workout.getRest());
+            put("Cycles", () -> workout.getCycles());
+            put("Sets", () -> workout.getSets());
+            put("Rest between sets", () -> workout.getRestBetweenSets());
+            put("Cool Down", () -> workout.getCoolDown());
+        }};
+
+        System.out.println(map.get(key));
+        return map.get(key).get();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void saveWorkout(View view){
+        for(Map.Entry<String, Integer> entry : ids.entrySet()){
+            EditText text =  findViewById(entry.getValue());
+            modifyValue(entry.getKey(),Integer.parseInt(String.valueOf(text.getText())));
         }
     }
 }
